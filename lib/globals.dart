83de4879aab6c103dec9e274b90e6e654854dec8
yht <yht20050302@sjtu.edu.cn>
2025-02-02 00:53:11 +0800

    Add global navigatorKey

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,7 @@
 import 'package:logger/logger.dart';
+import 'package:flutter/material.dart';
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final Logger logger = Logger(); // 全局日志工具
 String serverUrl = "http://localhost:3000"; // 全局服务器地址
 String wsUrl = "ws://localhost:3000";
